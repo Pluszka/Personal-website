c8660c6edd1b1a93df4326ec2f1da7f83973a2ca
Pliszka <karoliniakkjk@gmail.com>
2022-05-04 20:14:49 +0200

    working navigation

--- server.py
+++ server.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
+
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    if request.args.get('language') == 'pl':
+        return render_template('PLindex.html', lanuage='pl')
+    return render_template('index.html', lanuage='ang')
 
 if __name__ == "__main__":
     app.run(debug=True)
